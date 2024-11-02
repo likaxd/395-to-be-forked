b8c2a09d5c7b1c7b3c26554b94f14c5cc4b6ac0a
root <fenugreo@in-c1.nsk.21-school.ru>
2024-11-02 15:34:37 +0700

    users_structere БД пользователя 2/3 commit

--- БДПользователя/scripts/users_structure.sh
+++ БДПользователя/scripts/users_structure.sh
@@ -8,3 +8,11 @@ mkdir -p ../users/$nickname/photos/202{0..4}
 echo "Папки с 2020 по 2024 созданы"
 touch ../users/$nickname/photos/202{0..4}/default.png
 echo "default png cозданы"
+
+mkdir -p ../users/$nickname/videos
+echo "Папка videos сделана"
+mkdir -p ../users/$nickname/videos/202{0..4}
+echo "Папки с 2020 по 2024 созданы"
+touch ../users/$nickname/videos/202{0..4}/default.mp4
+echo "default mp4 cозданы"
+
