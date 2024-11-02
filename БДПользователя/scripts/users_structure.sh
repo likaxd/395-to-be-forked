adc37c2a451811efa0689e4e52ec0c228c7abb6f
root <fenugreo@in-c1.nsk.21-school.ru>
2024-11-02 15:33:01 +0700

    users_structere БД пользователя 1/3 commit

---
+++ БДПользователя/scripts/users_structure.sh
@@ -0,0 +1,10 @@
+mkdir -p ../users/
+
+read -p "Введите ник: " nickname
+
+mkdir -p ../users/$nickname/photos
+echo "Папка photos сделана"
+mkdir -p ../users/$nickname/photos/202{0..4}
+echo "Папки с 2020 по 2024 созданы"
+touch ../users/$nickname/photos/202{0..4}/default.png
+echo "default png cозданы"
